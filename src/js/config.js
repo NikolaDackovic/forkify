75f0271056c9133bf787982957e57cd9358bc049
NikolaDackovic <dackovicnikola1@gmail.com>
2025-11-23 01:08:31 +0100

    Initial commit - Forkify project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b85abe4e-8173-46d3-84f1-6b24f489085a';
+export const MODAL_CLOSE_SEC = 2.5;
